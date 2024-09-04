28ee6b0ebd9c4028e49f5bdfed38f95cb930877c
Evgeniy Dolmatov <dolmatov.e@optimax.dev>
2024-09-04 15:03:35 +0400

    some-fixes

--- app/Constants/CommonConstants.php
+++ app/Constants/CommonConstants.php
@@ -5,5 +5,4 @@ namespace App\Constants;
 interface CommonConstants
 {
     public const string TELEGRAM_BASE_URL = 'https://api.telegram.org/bot';
-    public const string TELEGRAM_BOT_URL = 'https://t.me/DevTest067Bot';
 }
