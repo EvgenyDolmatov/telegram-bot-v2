661bf7fb154a230095335926ed75d849ade33fe8
Evgeniy Dolmatov <dolmatov.e@optimax.dev>
2024-10-15 13:03:07 +0400

    refactoring-for-commands-NOT-READY

--- app/Commands/CommandContainer.php
+++ app/Commands/CommandContainer.php
@@ -3,13 +3,12 @@
 namespace App\Commands;
 
 use App\Enums\CommandEnum;
-use App\Enums\CommonCallbackEnum;
 
 class CommandContainer
 {
     public static function retrieve(string $commandIdentifier): CommandInterface
     {
-        $commands = array_column(CommonCallbackEnum::cases(), 'value');
+        $commands = array_column(CommandEnum::cases(), 'value');
         if (in_array($commandIdentifier, $commands)) {
             return CommandEnum::tryFrom($commandIdentifier)->getCommand();
         }
