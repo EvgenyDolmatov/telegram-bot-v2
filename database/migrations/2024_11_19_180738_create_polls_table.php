2bb0d35caf8770b3d3ce7d5fdd34d850b2abea1b
Evgeniy Dolmatov <evgeniy.webdesign@gmail.com>
2024-11-19 22:16:02 +0400

    TEAMTASK-74 Migration - add polls and poll_options tables

---
+++ database/migrations/2024_11_19_180738_create_polls_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('polls', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('tg_message_id');
+            $table->text('question');
+            $table->boolean('is_anonymous')->value(0);
+            $table->boolean('allows_multiple_answers')->value(1);
+            $table->enum('type', ['regular', 'quiz'])->default('regular');
+            $table->integer('correct_option_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('polls');
+    }
+};
