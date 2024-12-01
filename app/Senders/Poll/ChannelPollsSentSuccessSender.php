63faeadd46175cf12c60d2ea2aac65e5bcf726d7
Evgeniy Dolmatov <evgeniy.webdesign@gmail.com>
2024-12-01 08:26:03 +0400

    TEAMTASK-74 Send polls to channel (not ready)

--- app/Senders/Poll/ChannelPollsSentSuccessSender.php
+++ app/Senders/Poll/ChannelPollsSentSuccessSender.php
@@ -2,10 +2,14 @@
 
 namespace App\Senders\Poll;
 
+use App\Builder\Poll\PollBuilder;
 use App\Dto\ButtonDto;
 use App\Enums\CommandEnum;
 use App\Enums\StateEnum;
+use App\Models\Poll;
+use App\Repositories\RequestRepository;
 use App\Senders\AbstractSender;
+use Illuminate\Support\Facades\Log;
 
 class ChannelPollsSentSuccessSender extends AbstractSender
 {
@@ -13,9 +17,8 @@ class ChannelPollsSentSuccessSender extends AbstractSender
     {
         $this->addToTrash();
 
-
-        // TODO: write code
-
+        // Send polls to channel
+        $this->sendPollsToChannel();
 
         $text = StateEnum::CHANNEL_POLLS_SENT_SUCCESS->title();
         $buttons = [new ButtonDto("/" . CommandEnum::START->value, "Вернуться в начало")];
@@ -24,8 +27,9 @@ class ChannelPollsSentSuccessSender extends AbstractSender
         $this->senderService->sendMessage($message);
     }
 
-    private function normalizeChannelName(string $channelName): string
+    private function normalizeChannelName(): string
     {
+        $channelName = $this->getInput();
         if (str_contains($channelName, 'https://t.me/')) {
             return "@" . substr($channelName, 13);
         }
@@ -33,8 +37,44 @@ class ChannelPollsSentSuccessSender extends AbstractSender
         return '@' . ltrim($channelName, '@');
     }
 
-    private function sendToChannel(string $channelName): void
+    private function sendPollsToChannel(): void
     {
-        // TODO: write
+        $channelName = $this->getInput();
+        $preparedPolls = $this->user->preparedPolls()->first();
+
+        if ($preparedPolls) {
+            $pollIds = explode(',', $preparedPolls->poll_ids);
+
+            foreach ($pollIds as $pollId) {
+                if ($poll = Poll::where('tg_message_id', $pollId)->first()) {
+                    $options = [];
+
+                    foreach ($poll->options as $pollOption) {
+                        $options[] = $pollOption->text;
+                    }
+
+                    Log::debug('ChannelPollsSentSuccessSender: ' . $poll->question);
+
+                    $pollBuilder = $this->pollBuilder
+                        ->setBuilder(new PollBuilder())
+                        ->createPoll(
+                            question: $poll->question,
+                            options: $options,
+                            isAnonymous: (bool)$poll->isAnonymous,
+                            isQuiz: !$poll->allows_multiple_answers,
+                            correctOptionId: $poll->correct_option_id,
+                        );
+
+                    $this->senderService->sendPoll($pollBuilder, $channelName);
+                }
+            }
+        }
     }
+
+    private function getInput(): string
+    {
+        return (new RequestRepository($this->request))->getDto()->getText();
+    }
+
+    // TODO: Write method for getting channel id
 }
