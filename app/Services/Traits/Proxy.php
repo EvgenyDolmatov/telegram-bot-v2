522517c8d482c590bec9b9ed273424e30e0013e1
Evgeniy Dolmatov <dolmatov.e@optimax.dev>
2025-03-12 09:22:11 +0400

    Fix proxy

--- app/Services/Traits/Proxy.php
+++ app/Services/Traits/Proxy.php
@@ -4,7 +4,7 @@ namespace App\Services\Traits;
 
 trait Proxy
 {
-    const string PROXY_HOST = '192.166.81.141:8000';
-    const string PROXY_LOGIN = 'waHz92';
-    const string PROXY_PASSWORD = 'SmJaEd';
+    const string PROXY_HOST = '154.30.137.17:8000';
+    const string PROXY_LOGIN = 'ZnrRDH';
+    const string PROXY_PASSWORD = 'Z6zDbe';
 }
